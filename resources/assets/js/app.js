3a5dd8b9b26e87b94211bd0761a26aaf1355c777
febridwip <febridwipoethro@gmail.com>
2019-06-05 09:45:25 +0700

    profile.vue, dashboard.vue
    create file profile.vue, dashboard.vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,9 +12,13 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 let routes = [
-    { path: '/dashboard', component: Foo },
-    { path: '/profile', component: Bar }
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/profile', component: require('./components/Profile.vue') }
 ]
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
 
 /**
  * The following block of code may be used to automatically register your
