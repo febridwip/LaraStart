fb7528f2d8532593a8bf9f3b9353608f7afc4465
febridwip <febridwipoethro@gmail.com>
2019-06-05 09:30:35 +0700

    enter

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,14 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: Foo },
+    { path: '/profile', component: Bar }
+]
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
